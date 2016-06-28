459ef71affd179c9c5b272365afca66992c85940
liuyankun <lyankun1988@126.com>
2016-06-28 22:52:36 +0800

    context启动测试

---
+++ src/com/lyk/myspring/listener/InstantiationTracingBeanPostProcessor.java
@@ -0,0 +1,14 @@
+package com.lyk.myspring.listener;
+
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+
+public class InstantiationTracingBeanPostProcessor implements ApplicationListener<ContextRefreshedEvent>{
+	@Override
+	  public void onApplicationEvent(ContextRefreshedEvent event) {
+	      if(event.getApplicationContext().getParent() == null){//root application context 没有parent，他就是老大.
+	           
+	      }
+	      System.out.println("=======qidon");
+	  }
+}
--- src/com/lyk/myspring/user/service/UserService.java
+++ src/com/lyk/myspring/user/service/UserService.java
@@ -2,6 +2,8 @@ package com.lyk.myspring.user.service;
 
 import java.util.List;
 
+import javax.annotation.PostConstruct;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
