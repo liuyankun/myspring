d2bfde02270f88b4c57b108698a3bc4ba1970b81
liuyankun <lyankun1988@126.com>
2016-06-29 22:55:27 +0800

    Spring task

---
+++ src/com/lyk/myspring/task/TestTask.java
@@ -0,0 +1,17 @@
+package com.lyk.myspring.task;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TestTask {
+
+	private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
+	@Scheduled(cron = "*/10 * * * * *")
+    public void reportCurrentTime() {
+        System.out.println("The time is now " + dateFormat.format(new Date()));
+    }
+}
